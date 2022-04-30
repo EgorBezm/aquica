8bf40245a2337bbbed16ee23094f6a754e433d67
KremlinAgent <103261250+KremlinAgent@users.noreply.github.com>
2022-04-30 21:28:03 +0500

    Свертан сайтбар-юзер, кнопки навигации

--- resources/views/user/layots/sidebar.blade.php
+++ resources/views/user/layots/sidebar.blade.php
@@ -1,3 +1,43 @@
+<link rel="stylesheet" href="../resources/css/sidebar.css">  
 <div class="sidebar">
-    <h2>Это сайдбар</h2>
+    <div class="sidebar-user">
+    <div class="user-emoji">
+                <img src="..resources\img\avatars\avatar1.svg" alt="user">
+            </div>
+            <div class="user-welcome">
+                <h4>Hello,</h4>
+                <h2>Aquica User</h2>
+            </div>
+            <div class="user-settings">
+                <img src="..resources\img\icons\icon-settings.svg" alt="settings">
+            </div>
+    </div>К
+    <div class="sidebar-nav">
+        <div class="navbuttons">
+            <button class="sidebar_button">
+                <img src="" alt="nav-button">
+                <h4>Community</h4>
+                <img src="" alt="nav-button">
+            </button>   
+            <button class="sidebar-button">
+                <img src="" alt="nav-button">
+                <h4>Storage</h4>
+                <img src="" alt="nav-button">
+            </button>
+            <button class="sidebar-button">
+                <img src="" alt="nav-button">
+                <h4>Admin Panel</h4>
+                <img src="" alt="nav-button">
+            </button>
+        </div>
+    </div>
+    <div class="sidebar-content">
+
+    </div>
+    <div class="sidebar-signout">
+            <div class="sidebar-signout_button">
+                <img src="" alt="signout">
+                <p>Sign Out</p>
+            </div>
+        </div>
 </div>
