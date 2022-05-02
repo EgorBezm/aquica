14a0dfd056f6e77540f2c58f620b4f7bc0f78c74
Egor Bezmaternykh <bea16714@gmail.com>
2022-05-03 03:49:00 +0500

    Add page login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,30 +1,42 @@
-@extends('layouts.master')
+@extends('login.layouts.login-master')
 
 @section('header')
-    {{--  здесь кнопки  --}}
+
 @stop
 
 @section('content')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
-@stop
+    <div class="login">
+        <form action="" method="">
+            <div class="login__logo">
+                <img src="{{ asset('images/logos/logo-bg-blue.png') }}" alt="logo">
+            </div>
+            <div class="login__description">
+                Login to Aquica
+            </div>
 
-@section('footer')
-    {{-- Здесь должно быть пусто --}}
-@stop@extends('layouts.master')
+            <div class="for-input for-input_email">
+                <input type="email" name="email" placeholder="Email">
+                <button>
+                    <img src="{{ asset('images/icons/icon-send.png') }}" alt="send">
+                </button>
+            </div>
+            <div class="for-input for-input_password">
+                <input type="password" name="password" placeholder="Password">
+                <button>
+                    <img src="{{ asset('images/icons/icon-send.png') }}" alt="send">
+                </button>
+            </div>
+            <div class="forgot">
+                <hr class="forgot__hr">
+                <a href="#" class="forgot__link">
+                    Forgot password?
+                </a>
+            </div>
 
-@section('header')
-    {{--  здесь кнопки  --}}
-@stop
-
-@section('content')
-    @include('user.layots.sidebar')
-
-    <div class="content__main">
-
+        </form>
     </div>
-@stop
-
-@section('footer')
 
 @stop
 
