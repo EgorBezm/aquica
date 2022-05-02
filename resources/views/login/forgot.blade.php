8f407226b94c747d376c36396f0faf5fc668dad0
Egor Bezmaternykh <bea16714@gmail.com>
2022-05-03 02:12:58 +0500

    Add page forgot

--- resources/views/login/forgot.blade.php
+++ resources/views/login/forgot.blade.php
@@ -3,8 +3,20 @@
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/forgot.css') }}">
 
-    <div class="wrapper">
-
+    <div class="forgot">
+        <div class="forgot__name">
+            Aquica
+        </div>
+        <h1 class="forgot__main-text">
+            Having trouble signing in to your account?
+        </h1>
+        <div class="forgot__description">
+            Enter your email to get started account recovery.
+        </div>
+        <form action="" method="">
+            <input type="email" name="email" placeholder="Email"><br>
+            <button class="btn btn_blue">Processed</button>
+        </form>
     </div>
 
 @stop
